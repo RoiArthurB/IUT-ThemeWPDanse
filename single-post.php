285bce1bcfce83a0c560ee357255c2e52405803d
Arthur Brugiere <arbr1@hotmail.fr>
2016-03-28 13:06:05 +0200

    Design single-post (bug fction)

--- single-post.php
+++ single-post.php
@@ -1,20 +1,25 @@
 <?php get_header(); ?>
-	<div class="thumbnail dancer">
-		<?php if(has_post_thumbnail($post->ID)) : ?>
-			<?php echo get_the_post_thumbnail( $post->ID, 'normal' ); ?>
-		<?php else:?>
-
-			<?php echo '<img src="' . get_template_directory_uri (). '/images/photo_no_available_dance.jpg" />'; ?>
-		<?php endif;?>
-	</div>
-	<div class="panel panel-default">
-	  <div class="panel-heading">
-	    <h3 class="panel-title"><?php the_title( "<h3 class=''>", "</h3>" ); ?></h3>
-	  </div>
-	  <div class="panel-body">
-		<?php the_taxonomies();?>
-	    <?php the_content( ); ?>
-	  </div>
+	<div class="row">
+		<div class="col-sm-12 col-md-9">
+			<div class="text-center" style="text-transform: uppercase;"><?php the_category(', ');?></div>
+			<h3 class="text-center" style="text-transform: uppercase;"><?php the_title(); ?></h3>
+			<div class="text-center">
+				<span>written by <?php the_author(); //TODO Pk ça marche pas? :( ) ?></span>
+				<span>|</span>
+				<span><?php echo get_the_date(); ?></span>
+			</div>
+			<?php if(has_post_thumbnail($post->ID)) : ?>
+			<div class="text-center" style="padding: 10px">
+				<?php echo get_the_post_thumbnail( $post->ID, 'normal' ); ?>
+			</div>
+			<?php endif;?>
+			<div class="panel-body">
+				<?php the_content() ?>
+			</div>
+		</div>
+		<div id="sidebar" class="col-md-3 hidden-xs hidden-sm">
+			<?php get_sidebar();?>
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
