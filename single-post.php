be19a1f1b09c0b0449b909c44566f3a29e2f6754
Arthur B <Arthur B>
2016-04-01 14:45:04 +0200

    contenu article

--- single-post.php
+++ single-post.php
@@ -1,15 +1,24 @@
+<?php
+/**
+ * The main template file
+ */
+?>
 <?php get_header(); ?>
-	<div class="row">
+<div class="row">
+<?php if ( have_posts() ) : ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<div class="col-md-12">	
+
 		<div class="col-sm-12 col-md-9">
 			<div class="text-center" style="text-transform: uppercase;"><?php the_category(', ');?></div>
 			<h3 class="text-center" style="text-transform: uppercase;"><?php the_title(); ?></h3>
 			<div class="text-center">
-			
+
 			<?php if ( get_the_author() && get_the_date() ) : ?>
 				<p>written by <?php the_author(); ?> | <?php echo get_the_date() ?></p>
 			<?php elseif ( get_the_author() ) : ?>
 				<p>written by <?php the_author(); ?></p>
-			<?php elseif ( get_the_date() ) ?>
+			<?php elseif ( get_the_date() ) : ?>
 				<p>written the <?php echo get_the_date(); ?></p>
 			<?php endif; ?>
 
@@ -23,9 +32,15 @@
 				<?php the_content() ?>
 			</div>
 		</div>
-		<div id="sidebar" class="col-md-3 hidden-xs hidden-sm">
-			<?php get_sidebar();?>
+
 		</div>
+	<?php endwhile; ?>
+<?php else : ?>
+	<div class="col-md-12">
+	<?php 
+		get_template_part( 'template-parts/content', 'none' );
+	?>
 	</div>
+<?php endif; ?>
 </div>
-<?php get_footer(); ?>
+<?php get_footer();?>
