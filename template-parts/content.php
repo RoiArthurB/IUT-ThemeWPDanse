ce524e02e1e83342135bfe657c1e57123ee5dc84
Arthur Brugiere <arbr1@hotmail.fr>
2016-04-02 19:09:20 +0200

    bugfix de l'infini + peaufinage de l'index

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,17 +1,15 @@
 <div class="panel panel-default">
 	<a href="<?php echo get_permalink(); ?>" style="text-decoration: none;">
-		<div class="media">
-			<div class="media-left">
+		<div class="text-center" style="overflow: hidden; height: 40vh;">
 				<?php if ( has_post_thumbnail($post->ID) ) : ?>
-					<?php the_post_thumbnail('cover'); ?>
+					<?php the_post_thumbnail('style="display: block; width: 100%; height: 100%;"'); ?>
 				<?php else : ?>
 					<!-- <img src="https://unsplash.it/200/?random" />-->
-					<img src="<?php echo get_template_directory_uri(); ?>/images/no_photo_post.jpg" />
+					<img src="<?php echo get_template_directory_uri(); ?>/images/no_photo_post.jpg" style="display: block; width: 430px; height: 430px;"/>
 				<?php endif; ?>
-			</div>
-			<div class="text-center media-body" style="vertical-align: middle;">
-				<h3 class='panel-title vcenter'><?php the_title();?></h3>
-			</div>
+		</div>
+		<div class="text-center" style="vertical-align: middle;">
+			<h3 class='panel-title vcenter'><?php the_title();?></h3>
 		</div>
 	</a>
 	<hr>
